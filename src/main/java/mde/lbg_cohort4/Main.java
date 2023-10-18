38b8996261de92bcac83094fcc3736eda3f417b1
conormurph1 <conorm2014@gmail.com>
2023-10-18 13:08:27 +0100

    Calculation and inputs

--- src/main/java/mde/lbg_cohort4/Main.java
+++ src/main/java/mde/lbg_cohort4/Main.java
@@ -9,8 +9,18 @@ public class Main {
         take_input();
     }
     public static void take_input(){
+        float amount;
+        float VAT_rate;
+        float total;
+        String again;
         Scanner sc = new Scanner(System.in);
-        System.out.println("VAT Calculator");
+        do{
+            System.out.println("Please enter the amount");
+            amount = sc.nextFloat();
+            System.out.println("Please enter the VAT rate");
+            VAT_rate = sc.nextFloat();
+            total = amount*(100+VAT_rate)/100;
+        }while(true);
     }
 
 }

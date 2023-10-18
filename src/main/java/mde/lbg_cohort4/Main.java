2efcf675006ca595e25b66a4bf1649bc3cd37a6c
conormurph1 <conorm2014@gmail.com>
2023-10-18 12:55:22 +0100

    Commit 1

--- src/main/java/mde/lbg_cohort4/Main.java
+++ src/main/java/mde/lbg_cohort4/Main.java
@@ -1,9 +1,16 @@
 package mde.lbg_cohort4;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        take_input();
+    }
+    public static void take_input(){
+        Scanner sc = new Scanner(System.in);
         System.out.println("VAT Calculator");
     }
+
 }
